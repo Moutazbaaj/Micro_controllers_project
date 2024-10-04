eaa4bdac547290d7ee9cb2f362a3fe86d83a52f0
Moutaz baaj <moutazbaag@gmail.com>
2024-10-04 18:34:06 +0200

    switch button added

--- RYGB_Led_with_joystick_control/RYGB_Led_with_joystick_control.ino
+++ RYGB_Led_with_joystick_control/RYGB_Led_with_joystick_control.ino
@@ -8,103 +8,110 @@ const int joyY = A1;
 
 int joyR = 6;
 
+const int buttonPin = 7; 
+
 bool randomMode = false;
+bool lightsOn = false; 
 unsigned long startTime; 
 
-
-
 void setup() {
+  pinMode(ledRed, OUTPUT);  
+  pinMode(ledYellow, OUTPUT);
+  pinMode(ledGreen, OUTPUT);
+  pinMode(ledBlue, OUTPUT);
+  pinMode(joyR, INPUT_PULLUP);
+  pinMode(buttonPin, INPUT_PULLUP); 
 
-pinMode(ledRed, OUTPUT);  
-pinMode(ledYellow, OUTPUT);
-pinMode(ledGreen, OUTPUT);
-pinMode(ledBlue, OUTPUT);
-pinMode(joyR, INPUT_PULLUP);
+  Serial.begin(9600);
 
-Serial.begin(9600);
-
-
-digitalWrite(ledRed, LOW);
-digitalWrite(ledYellow, LOW);
-digitalWrite(ledGreen, LOW);
-digitalWrite(ledBlue, LOW);
-
+  digitalWrite(ledRed, LOW);
+  digitalWrite(ledYellow, LOW);
+  digitalWrite(ledGreen, LOW);
+  digitalWrite(ledBlue, LOW);
 }
 
 void loop() {
+  int xValue = analogRead(joyX);
+  int yValue = analogRead(joyY);
+  int buttonState = digitalRead(joyR);
+  int buttonToggleState = digitalRead(buttonPin);
 
-int xValue = analogRead(joyX);
-int yValue = analogRead(joyY);
-int buttonState = digitalRead(joyR);
-
-if (buttonState == LOW && !randomMode) {
-  randomMode = true;
-  startTime = millis();
-   Serial.println("RandomMode On");
-}
-
-if (randomMode) {
-  if (millis() - startTime < 5000) {
-    randomBlink();
-  } else {
-    randomMode = false;
-
-   digitalWrite(ledRed, LOW);
-   digitalWrite(ledYellow, LOW);
-   digitalWrite(ledGreen, LOW);
-   digitalWrite(ledBlue, LOW);
-
+  static bool lastButtonState = HIGH; 
+  if (buttonToggleState == LOW && lastButtonState == HIGH) {
+    lightsOn = !lightsOn; 
+    Serial.println(lightsOn ? "Lights On" : "Lights Off");
+    delay(200); 
   }
-  return;
-}
+  lastButtonState = buttonToggleState; 
 
-if (xValue < 300) {
-  digitalWrite(ledRed, HIGH);
-  Serial.println("Red Light On");
-} else {
-  digitalWrite(ledRed, LOW);
+  // Handle random mode
+  if (buttonState == LOW && !randomMode && lightsOn) { // Check if lights are on
+    randomMode = true;
+    startTime = millis();
+    Serial.println("RandomMode On");
+  }
 
-}
+  if (randomMode) {
+    if (millis() - startTime < 5000) {
+      randomBlink();
+    } else {
+      randomMode = false;
+      turnOffAllLeds();
+    }
+    return;
+  }
 
-if (xValue > 700) {
-  digitalWrite(ledYellow, HIGH);
-  Serial.println("Yellow Light On");
-} else {
-  digitalWrite(ledYellow, LOW);
+  // Only control LEDs if lights are on
+  if (lightsOn) {
+    if (xValue < 300) {
+      digitalWrite(ledRed, HIGH);
+      Serial.println("Red Light On");
+    } else {
+      digitalWrite(ledRed, LOW);
+    }
 
-}
+    if (xValue > 700) {
+      digitalWrite(ledYellow, HIGH);
+      Serial.println("Yellow Light On");
+    } else {
+      digitalWrite(ledYellow, LOW);
+    }
 
-if (yValue < 300) {
-  digitalWrite(ledGreen, HIGH);
-  Serial.println("Green Light On");
-} else {
-  digitalWrite(ledGreen, LOW);
+    if (yValue < 300) {
+      digitalWrite(ledGreen, HIGH);
+      Serial.println("Green Light On");
+    } else {
+      digitalWrite(ledGreen, LOW);
+    }
 
-}
+    if (yValue > 700) {
+      digitalWrite(ledBlue, HIGH);
+      Serial.println("Blue Light On");
+    } else {
+      digitalWrite(ledBlue, LOW);
+    }
+  }
 
-if (yValue > 700) {
-  digitalWrite(ledBlue, HIGH);
-  Serial.println("Blue Light On");
-} else {
-  digitalWrite(ledBlue, LOW);
-
-}
-
-delay(100);
+  delay(100);
 }
 
 void randomBlink() {
-  // Generate random numbers (0 or 1) for each LED
-  int redState = random(0, 2);    // Randomly turn Red LED on/off
-  int yellowState = random(0, 2); // Randomly turn Yellow LED on/off
-  int greenState = random(0, 2);  // Randomly turn Green LED on/off
-  int blueState = random(0, 2);   // Randomly turn Blue LED on/off
+  int redState = random(0, 2);
+  int yellowState = random(0, 2);
+  int greenState = random(0, 2);
+  int blueState = random(0, 2);
 
-  // Set the LEDs to random states
   digitalWrite(ledRed, redState);
   digitalWrite(ledYellow, yellowState);
   digitalWrite(ledGreen, greenState);
   digitalWrite(ledBlue, blueState);
 
-  delay(200); // Small delay for the blinking effect
+  delay(200);
+}
+
+void turnOffAllLeds() {
+  digitalWrite(ledRed, LOW);
+  digitalWrite(ledYellow, LOW);
+  digitalWrite(ledGreen, LOW);
+  digitalWrite(ledBlue, LOW);
 }
